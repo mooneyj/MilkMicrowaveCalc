23b4627aea12f57814479c85b71014c99afbd4ef
John Mooney <mooneyj@users.noreply.github.com>
2024-07-20 16:37:29 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
-// service-worker.js
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'milk-warmer-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/script.js',
-        '/icon.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
